d122d6c02defec24197acd4de48722c8bda04740
William L. DeRieux IV <WilliamDeRieux@gmail.com>
2023-09-18 04:52:30 -0400

    Fixed cmake compilation errors.
    * Explicitly add source files (do not use GLOB) and
      add windows specific sources/libraries (if WIN32).
    
    * The udis library should be udis86.
    
    * The mhash_config.h file is located in the mutils
      sub-directory, so look for it there.
    
    * The defined PACKAGE_STRING value should always be
      set in udcli.c and not be only if MSC_VER is set.
    
    * The config.h file, in udcli.c, should not be included
      if HAVE_CONFIG_H is not set.

--- udis86/udcli/udcli.c
+++ udis86/udcli/udcli.c
@@ -23,16 +23,18 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+ #define PACKAGE_STRING "udis86 pre-1.8"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #ifdef _MSC_VER
 #include "..\udis86.h"
-#define PACKAGE_STRING "udis86 pre-1.8"
 #else
 #include <udis86.h>
+#ifdef HAVE_CONFIG_H
 #include <config.h>
+#endif /* HAVE_CONFIG_H */
 #endif
 
 #if defined(__APPLE__)
